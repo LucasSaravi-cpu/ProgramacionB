dcb56e51b90faa7d97f1dbba58b9a14a14d7dd99
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2023-05-05 16:11:46 -0300

    Add files via upload

---
+++ Guia2Ejercicio1/Figura.java
@@ -0,0 +1,15 @@
+
+public abstract class Figura {
+   
+	
+
+	public abstract double getArea();
+	
+	public abstract double getPerimetro();
+	
+	public abstract String getInfoAtributos();
+	
+	public abstract String getTipoFigura (); 
+	
+	
+}
---
+++ Guia2Ejercicio1/Rectangulo.java
@@ -0,0 +1,54 @@
+public class Rectangulo extends Figura {
+
+ float base ,altura;
+ 
+
+
+
+public Rectangulo(float base, float altura) { //Constructores de nuestros atributos
+	super();
+	this.base = base;
+	this.altura = altura;
+}
+
+
+public float getBase() {
+	return base;
+}
+
+
+public float getAltura() {
+	return altura;
+}
+
+
+@Override
+public  double getArea() {
+  
+	return base*altura;
+	
+}
+
+@Override
+public  double getPerimetro() {
+	
+	return base+base+altura+altura;
+}
+
+@Override
+public  String getInfoAtributos() {
+	
+	return "Su base es igual a " +base+"y su altura es igual a " +altura;
+}
+
+
+@Override
+public  String getTipoFigura () {
+	
+	return "Es un Rectangulo";
+}
+ 
+}
+
+	
+
