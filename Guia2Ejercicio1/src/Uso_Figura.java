fbb07b09b117d4212c3eb6dcc063b236f620a657
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2023-05-13 02:21:59 -0300

    Add files via upload

---
+++ Guia2Ejercicio1/src/Uso_Figura.java
@@ -0,0 +1,27 @@
+public class Uso_Figura {
+
+	 public static void main(String[] args) {
+
+	 
+	 Figura MisFiguras[]=new Figura[6]; 
+	
+    
+		MisFiguras[0] = new Cuadrado(45,45);
+		MisFiguras[1] = new Circulo(45,45);
+		MisFiguras[2] = new Circulo(20,20);
+		MisFiguras[3] = new Rectangulo(10,45);
+		MisFiguras[4] = new Elipce(45,25);
+		MisFiguras[5] = new Cuadrado(5,5);
+		
+	
+   
+    	 
+	 for (int i=0 ; i<MisFiguras.length ; i++) {
+		  
+		 System.out.println("Esta figura "+MisFiguras[i].getTipoFigura()+" y sus atributos son : " +MisFiguras[i].getInfoAtributos()+" su area es  "+MisFiguras[i].getArea()+ " y su perimetro es de " + MisFiguras[i].getPerimetro()) ;
+		  
+	
+	}
+}
+}
+
