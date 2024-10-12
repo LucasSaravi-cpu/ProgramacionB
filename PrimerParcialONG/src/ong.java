134082138f44e3613e36e512efecb098e3494c12
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2024-10-12 12:22:01 -0300

    Add files via upload

---
+++ PrimerParcialONG/src/ong.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+
+
+public class ong {
+	
+	 private String nombre;
+	 private ArrayList<Colaborador> colaboradores;
+	 private ArrayList<Colaboracion> colaboraciones;
+	 private double totalAportes;
+	 private double totalColaboraciones;
+	
+
+	public ong(String nombre) {
+		super();
+		this.nombre = nombre;
+		colaboradores = new ArrayList<>();
+		colaboraciones = new ArrayList<>();
+		totalAportes=0;
+		totalColaboraciones=0;
+		
+		
+	}
+
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return nombre ;
+	}
+
+
+
+
+	public ArrayList<Colaborador> getColaboradores() {
+		return colaboradores;
+	}
+
+
+
+
+	public void setColaboradores(ArrayList<Colaborador> colaboradores) {
+		this.colaboradores = colaboradores;
+	}
+
+
+
+
+	public ArrayList<Colaboracion> getColaboraciones() {
+		return colaboraciones;
+	}
+
+
+
+
+	public void setColaboraciones(ArrayList<Colaboracion> colaboraciones) {
+		this.colaboraciones = colaboraciones;
+	}
+
+
+
+
+
+	   
+    public void calcularTotales() {
+   
+    	
+        for (Colaborador colaborador : colaboradores) {
+            if (colaborador.TipoColaborador().equals("VOLUNTARIO")) {
+                totalColaboraciones += colaborador.getCosto();
+              
+            } else {
+                totalAportes += colaborador.getCosto();
+            }
+        }
+    }
+
+    
+    public double TotalDeAportes() {
+    
+        return  totalAportes + totalColaboraciones  ;
+    }
+
+
+
+
+	public double getTotalAportes() {
+		return totalAportes;
+	}
+
+
+
+
+	public void setTotalAportes(double totalAportes) {
+		this.totalAportes = totalAportes;
+	}
+	 
+
+	
+	 
+	 
+	 
+
+}
